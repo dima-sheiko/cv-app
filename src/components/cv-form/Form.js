e1d0d196ade19c1c1ba36bf4bcf44dfaaef23f4c
dima-sheiko <sheydim@gmail.com>
2022-06-30 17:53:33 +0300

    Refactor the button

--- src/components/cv-form/Form.js
+++ src/components/cv-form/Form.js
@@ -1,9 +1,7 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Personal from './Personal';
 import Education from './Education';
 import Experience from './Experience';
-import ReactToPrint from 'react-to-print';
-import Resume from '../cv-preview/Resume';
 
 function Form({
   personal,
@@ -17,7 +15,6 @@ function Form({
   handleEducationChange,
   handleExperienceChange,
 }) {
-  const componentRef = useRef();
   return (
     <div className='form-wrapper'>
       <form className='form'>
@@ -39,11 +36,7 @@ function Form({
         />
       </form>
       <div className='form-footer'>
-        <ReactToPrint
-          trigger={() => <button className='form-btn green'>Print CV</button>}
-          content={() => componentRef.current}
-        />
-        <Resume ref={componentRef} />
+        <button className='form-btn green'>Print CV</button>
       </div>
     </div>
   );
